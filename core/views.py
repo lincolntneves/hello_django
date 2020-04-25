58ecd13d4781acc6afc3a45012a1c6520b2710b8
lincolntneves <lincolnthiago.neves@gmail.com>
2020-04-25 13:27:19 -0300

    Commit da primeira aplicação Django. Retornando um Hello World

--- core/views.py
+++ core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    tot = int(num1) + int(num2)
+    return HttpResponse('O total é: {}'.format(tot))
+
